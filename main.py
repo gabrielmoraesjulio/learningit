666661e904ada8dcd388295fb12a9cac4c936df2
Gabriel <golk76@outlook.com>
2025-03-12 08:49:50 -0300

    Creating the phantoms dictionary :D

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
-name = "Joker"
+phanthoms = {
+    "Joker": {
+        "name": "Ren Amamiya",
+        "age": 17,
+        "arcana": "Fool",
+        "persona": "Arsène"
+    },
+    "Mona": {
+        "name": "Morgana",
+        "age": "???",
+        "arcana": "Magician",
+        "persona": "Zorro"
+    },
+    "Skull": {
+        "name": "Ryuji Sakamoto",
+        "age": 17,
+        "arcana": "Chariot",
+        "persona": "Captain Kidd"
+    },
+}
 
-print(f"Hello {name}!")
+
+print("Informations about the Phanthom Thieves of Hearts...")
+for phanthoms, data in phanthoms.items():
+    for key, value in data.items():
+        print(f" {key}: {value}")
