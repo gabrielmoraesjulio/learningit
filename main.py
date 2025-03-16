444cc5df457665f25435c61a26cebb83cadba9bb
Gabriel <golk76@outlook.com>
2025-03-16 10:06:41 -0300

    Persona attributes

--- main.py
+++ main.py
@@ -3,19 +3,91 @@ phanthoms = {
         "name": "Ren Amamiya",
         "age": 17,
         "arcana": "Fool",
-        "persona": "Arsène"
+        "persona": {
+            "name": "Arsène",
+            "awaken": "Satanael"
+        }
     },
     "Mona": {
         "name": "Morgana",
         "age": "???",
         "arcana": "Magician",
-        "persona": "Zorro"
+        "persona": {
+            "name": "Zorro",
+            "awaken": "Mercurius"
+        }
     },
     "Skull": {
         "name": "Ryuji Sakamoto",
         "age": 17,
         "arcana": "Chariot",
-        "persona": "Captain Kidd"
+        "persona": {
+            "name": "Captain Kidd",
+            "awaken": "Seiten Taisei"
+        }
+    },
+    "Panther": {
+        "name": "Anne Takamaki",
+        "age": 17,
+        "arcana": "Lovers",
+        "persona": {
+            "name": "Carmen",
+            "awaken": "Hecate"
+        }
+    },
+    "Fox": {
+        "name": "Yusuke Kitagawa",
+        "age": 17,
+        "arcana": "Emperor",
+        "persona": {
+            "name": "Goemon",
+            "awaken": "Kamu Susano-o"
+        }
+    },
+    "Queen": {
+        "name": "Makoto Niijima",
+        "age": 17,
+        "arcana": "Priestess",
+        "persona": {
+            "name": "Johanna",
+            "awaken": "Anat"
+        }
+    },
+    "Oracle": {
+        "name": "Futaba Sakura",
+        "age": 17,
+        "arcana": "Hermit",
+        "persona": {
+            "name": "Necronomicon",
+            "awaken": "Prometheus"
+        }
+    },
+    "Noir": {
+        "name": "Haru Okumura",
+        "age": 17,
+        "arcana": "Empress",
+        "persona": {
+            "name": "Milady",
+            "awaken": "Astarte"
+        }
+    },
+    "Crow": {
+        "name": "Goro Akechi",
+        "age": 17,
+        "arcana": "Justice",
+        "persona": {
+            "name": "Loki",
+            "awaken": "Hereward"
+        }
+    },
+    "Violet": {
+        "name": "Sumire Yoshizawa",
+        "age": 15,
+        "arcana": "Faith",
+        "persona": {
+            "name": "Cendrillon",
+            "awaken": "Vanadis"
+        }
     },
 }
 
@@ -24,4 +96,9 @@ print("Informations about the Phanthom Thieves of Hearts...")
 for phanthoms, data in phanthoms.items():
     print(phanthoms)
     for key, value in data.items():
-        print(f" {key}: {value}")
+        if isinstance(value, dict):
+            print(f" {key}:")
+            for persona_key, persona_value in value.items():
+                print(f"    {persona_key}: {persona_value}")
+        else:
+            print(f" {key}: {value}")
